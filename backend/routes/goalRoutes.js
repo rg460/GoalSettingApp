470f24bb17a43ec6d33476fb76d082efe8a9caa8
ravigordon <rgordz460@outlook.com>
2022-05-26 17:53:36 +0100

    post , put and delete routes set up

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,5 +4,14 @@ const router = express.Router();
 router.get('/', (req, res) =>{
     res.status(200).json({message:'Get goals'});
 })
+router.post('/', (req, res) =>{
+    res.status(200).json({message:'Set goals'});
+})
+router.put('/:id', (req, res) =>{
+    res.status(200).json({message:`Update goal ${req.params.id}`});
+})
+router.delete('/', (req, res) =>{
+    res.status(200).json({message:`Update goal ${req.params.id}`});
+})
 
 module.exports = router
