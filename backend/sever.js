5d44f2c917f4ddf3ad970ca5d09eb3a78ca8f362
ravigordon <rgordz460@outlook.com>
2022-05-26 16:47:57 +0100

    get request set up

--- backend/sever.js
+++ backend/sever.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();   
 
-const port =process.env.PORT || 5000;
+const port =process.env.PORT || 3000;
 
 const app = express();
+
+app.get('/api/goals', (req, res) =>{
+    res.json({message:'Get goals'});
+})
 
 app.listen(port , () => console.log(`server listening on ${port}`));
