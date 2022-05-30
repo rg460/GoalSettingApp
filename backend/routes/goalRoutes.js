f36e3d36ab42070f55b95786e6c3f53855d33c15
ravigordon <rgordz460@outlook.com>
2022-05-30 10:42:19 +0100

    set , update and delete refactored

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {getGoals} = require('../controllers/goalController')
+const {setGoal} = require('../controllers/goalController')
+const {updateGoal} = require('../controllers/goalController')
+const {deleteGoal} = require('../controllers/goalController')
+
 
 router.get('/', getGoals)
-router.post('/', (req, res) =>{
-    res.status(200).json({message:'Set goals'});
-})
-router.put('/:id', (req, res) =>{
-    res.status(200).json({message:`Update goal ${req.params.id}`});
-})
-router.delete('/:id', (req, res) =>{
-    res.status(200).json({message:`Delete goal ${req.params.id}`});
-})
+router.post('/',  setGoal)
+router.put('/:id', updateGoal)
+router.delete('/:id', deleteGoal)
 
 module.exports = router
 
