b27ba8dc5b0caa3ed98b7f95afe31b4b79513f20
ravigordon <rgordz460@outlook.com>
2022-05-30 10:52:16 +0100

    goal routes file refactored further

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
+router.route('/').get(getGoals).post(setGoal)
 
-router.get('/', getGoals)
-router.post('/',  setGoal)
 router.put('/:id', updateGoal)
 router.delete('/:id', deleteGoal)
 
