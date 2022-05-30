efd6898394af54080ff1868b79d4579a1149c8cf
ravigordon <rgordz460@outlook.com>
2022-05-30 11:13:50 +0100

    request to text in body working in postman

--- backend/sever.js
+++ backend/sever.js
@@ -5,7 +5,7 @@ const port =process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded)
+app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
 
