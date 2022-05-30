62994a01cd90822afd0b3a15f67557ff3d88f535
ravigordon <rgordz460@outlook.com>
2022-05-30 14:21:42 +0100

    error handler called in server,js

--- backend/sever.js
+++ backend/sever.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv').config();   
+const dotenv = require('dotenv').config(); 
+const {errorHandler}  = require('./middleware/errorMiddleware')
 const port =process.env.PORT || 3000;
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+app.use(errorHandler)
 
 app.listen(port , () => console.log(`server listening on ${port}`));
 
