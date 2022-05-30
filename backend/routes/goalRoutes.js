2478fe82baf1b6f2c8b55545160eb3b088bcb48c
ravigordon <rgordz460@outlook.com>
2022-05-30 10:55:35 +0100

    delete and put refactored

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
 router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
-router.put('/:id', updateGoal)
-router.delete('/:id', deleteGoal)
+
 
 module.exports = router
 
