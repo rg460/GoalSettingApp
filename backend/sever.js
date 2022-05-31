60a70f1d6a2dfa3337c06fd4aa82a0c06c2d8e20
ravigordon <rgordz460@outlook.com>
2022-05-31 15:24:22 +0100

    connectDB brought into server.js

--- backend/sever.js
+++ backend/sever.js
@@ -2,7 +2,10 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config(); 
 const {errorHandler}  = require('./middleware/errorMiddleware')
+const conectDB = require('./config/db')
 const port =process.env.PORT || 3000;
+
+conectDB()
 
 const app = express();
 
