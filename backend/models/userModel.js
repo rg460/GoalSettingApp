20496b0e0d21dbd74242df47b369852066d527e6
ravigordon <rgordz460@outlook.com>
2022-06-01 12:33:56 +0100

    email and password added to the userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,9 +4,14 @@ const userSchema = mongoose.schema({
     name:{
         type: String,
         required: [true,'Please add a name']
+    },email:{
+        type: String,
+        required: [true,'Please add an email address'],
+        unique: true
+    },password:{
+        type: String,
+        required: [true,'Please add a password']
     }
-},{
-    timestamp: true
 })
 
 module.exports = mongoose.model('Goal',userSchema)
