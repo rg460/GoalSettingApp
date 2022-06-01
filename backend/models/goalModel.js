a22d383c8cdf6568da730b80bad6617a16cbef40
ravigordon <rgordz460@outlook.com>
2022-06-01 10:06:07 +0100

    typo of mongoose.Schema fixed server is now running again

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const goalSchema =  mongose.Schema({
+const goalSchema =  mongoose.Schema({
     text:{
         type: String,
         required: [true,'Please add a text value']
