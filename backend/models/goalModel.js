f78c10e268f058a2af2abd060fd65e8446108107
ravigordon <rgordz460@outlook.com>
2022-06-03 10:34:27 +0100

    reference added to user object

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const goalSchema =  mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref: 'User'
     },
     text:{
         type: String,
