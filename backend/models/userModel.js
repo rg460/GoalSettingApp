6e0b6931f433e94cdf77998774b7dd2b03ea6ead
ravigordon <rgordz460@outlook.com>
2022-06-03 14:16:53 +0100

    error handling for name , email and password added to registerUser

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.schema({
-    name:{
+const userSchema = mongoose.Schema(
+    {
+      name: {
         type: String,
-        required: [true,'Please add a name']
-    },email:{
+        required: [true, 'Please add a name'],
+      },
+      email: {
         type: String,
-        required: [true,'Please add an email address'],
-        unique: true
-    },password:{
+        required: [true, 'Please add an email'],
+        unique: true,
+      },
+      password: {
         type: String,
-        required: [true,'Please add a password']
+        required: [true, 'Please add a password'],
+      },
     },
-},{
-    timestamp: true
-})
+    {
+      timestamps: true,
+    }
+  )
 
 module.exports = mongoose.model('User',userSchema)
