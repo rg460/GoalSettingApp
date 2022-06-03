2c4523e02d402f2ebf41fe470a9b11467db3aeb4
ravigordon <rgordz460@outlook.com>
2022-06-03 10:39:23 +0100

    user route corrected

--- backend/sever.js
+++ backend/sever.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
-app.use('/api/goals',require('./routes/userRoutes.js'))
+app.use('/api/users',require('./routes/userRoutes.js'))
 app.use(errorHandler)
 
 app.listen(port , () => console.log(`server listening on ${port}`));
