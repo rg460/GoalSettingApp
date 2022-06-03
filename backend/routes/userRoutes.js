f3d4abc71bf3d52278393c378d8cd0732fac5d5a
ravigordon <rgordz460@outlook.com>
2022-06-03 13:57:37 +0100

    Post request for login working in PostMan and router changed to get so get request is also working in Postman

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,9 @@ const {registerUser,loginUser,getMe} = require('../controllers/userController')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.post('/', getMe)
+router.get('/me', getMe)
 
 
 module.exports = router
+
+// Post request for login working in PostMan and router changed to get so get request is also working in Postman 
