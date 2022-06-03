998ecc4e496e2037922b785abb6cccfdc3f32565
ravigordon <rgordz460@outlook.com>
2022-06-03 14:07:22 +0100

    asyncHandler and User brought into userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc Register new user
 // @route POST/api/users
