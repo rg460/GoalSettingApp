6de5f6f039c69ecd590ab56594df40497cd28cec
ravigordon <rgordz460@outlook.com>
2022-06-04 10:21:57 +0100

    if statement to check if user was created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,6 +29,17 @@ const registerUser =asyncHandler(async (req,res) => {
         email,
         password: hashedPassword
     })
+    // if the user was created
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data') 
+    }
 
     res.json({message:'Register user'})
 }) 
