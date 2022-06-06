5d64cc99476ec9a0b6aa64674718890f70414908
ravigordon <rgordz460@outlook.com>
2022-06-06 16:19:10 +0100

    authenticating the user bug now fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,90 +1,90 @@
+  
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-// @desc Register new user
-// @route POST/api/users
-// @access Public
-const registerUser =asyncHandler(async (req,res) => {
-    const {name, email, password} =req.body
-    if(!name || !email || !password){
-        res.status(400)
-        throw new Error('Please add all fields')
-    }
-    // Check to see if user exists
-    const userExits = await User.findOne({email})
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
 
-    if(userExits){
-        res.status(400)
-        throw new Error('User already exists')
-    }
-    // Hash Paswword
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
 
-    // create user
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
     })
-    // if the user was created
-    if(user){
-        res.status(201).json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            token:generateToken(user._id)
-        })
-    }else{
-        res.status(400)
-        throw new Error('Invalid user data') 
-    }
-}) 
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
 
-// @desc Authenticate a user
-// @route POST/api/users/login
-// @access Public
-const loginUser = asyncHandler(async (req,res) => {
-    const {email, password} = req.body
-    // Check for user email
-    const user = await User.findOne({ email })
-    
-    if (user && (await bcrypt.compare(password, user.password))) {
-        res.json({
-          _id: user.id,
-          name: user.name,
-          email: user.email,
-          token:generateToken(user._id)
-        })
-      } else {
-        res.status(400)
-        throw new Error('Invalid credentials')
-      }
-    }) 
+// @desc    Authenticate a user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
 
-// @desc Get user data
-// @route GET/api/users/me
-// @access Public
-const getMe = asyncHandler(async (req,res) => {
-    res.json({message:'User data display'})
-}) 
+  // Check for user email
+  const user = await User.findOne({ email })
 
-// Generate JWT token
-const generateToken = (id) =>{
-    return jwt.sign({id}, process.env.JWT_SECRET,{
-        expiresIn: '30d',
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
     })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
+})
+
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user)
+})
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
 }
-
 
 module.exports = {
-    registerUser,
-    loginUser,
-    getMe
+  registerUser,
+  loginUser,
+  getMe,
 }
-// error Handling working in Postman
-// Working in Postman and MongoDB compass
-// Mongo DB working again
-// Login credentials working in Postman
