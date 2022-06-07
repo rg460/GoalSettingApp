1f43de0481374de8276a70383cc64ea46275b95d
ravigordon <rgordz460@outlook.com>
2022-06-07 10:11:16 +0100

    catch error completed for if user is not authorised

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,8 +12,11 @@ const protect = asyncHandler(async(req,res,next)=>{
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             // Get user from the token
             req.user = await User.findById(decoded.id).select('-password')
+            next()
         } catch (error){
-
+            console.log(error);
+            res.status(401)
+            throw new Error('Not authorized')
         }
     }
 })
