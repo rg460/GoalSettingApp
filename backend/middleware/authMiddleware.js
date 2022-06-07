fccc6049bb1f44c87dd39a0408e0eb7ef33d44f0
ravigordon <rgordz460@outlook.com>
2022-06-07 10:06:25 +0100

    get user from the token completed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = asyncHandler(async(req,res,next)=>{
             // Verify token
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             // Get user from the token
-            
+            req.user = await User.findById(decoded.id).select('-password')
         } catch (error){
 
         }
