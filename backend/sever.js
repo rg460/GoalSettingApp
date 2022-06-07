328bc49183984c99ffb97eba6fcd5fe3f1f47109
ravigordon <rgordz460@outlook.com>
2022-06-07 11:20:27 +0100

    API with Authentication Finished

--- backend/sever.js
+++ backend/sever.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config(); 
-const {errorHandler}  = require('./middleware/errorMiddleware')
-const conectDB = require('./config/db')
-const port =process.env.PORT || 3000;
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
 
-conectDB()
+connectDB()
 
-const app = express();
+const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals',require('./routes/goalRoutes.js'))
-app.use('/api/users',require('./routes/userRoutes.js'))
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+
 app.use(errorHandler)
 
-app.listen(port , () => console.log(`server listening on ${port}`));
-
+app.listen(port, () => console.log(`Server started on port ${port}`))
