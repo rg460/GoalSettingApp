d227d1d6252ce5276a0aa59256f5a29fdd5b89cd
ravigordon <rgordz460@outlook.com>
2022-06-08 10:59:50 +0100

    Header Component impoted into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
