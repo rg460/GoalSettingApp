4b1ec040f2d43f2538a11993fecaa31adfb514db
ravigordon <rgordz460@outlook.com>
2022-06-08 12:21:46 +0100

    state for form created

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,11 @@
 import {useState, useEffect} from 'react'
 function Register() {
+    const [formData, setFormData] = useState({
+        name:'',
+        email: '',
+        pasword: '',
+        password2: ''
+    })
   return (
     <div>Register</div>
   )
