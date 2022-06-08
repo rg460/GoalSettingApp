1fa71bc0fbadd636843e5c1fb9aa1fe180cd3f9d
ravigordon <rgordz460@outlook.com>
2022-06-08 10:20:46 +0100

    Login and Register imported to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 function App() {
   return (
     <>
