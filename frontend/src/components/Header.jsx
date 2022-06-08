c869d69bda2e9c47adfd76ec641f11ebf00e2c69
ravigordon <rgordz460@outlook.com>
2022-06-08 10:44:40 +0100

    link imported from react-router-dom

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import {} from 'react-icons/fa'
+import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
+import {link} from 'react-router-dom'
 function Header() {
   return (
     <div>Header</div>
