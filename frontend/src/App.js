05b5485481ef5f09a86fb0b32cc50f530ff55e97
ravigordon <rgordz460@outlook.com>
2022-06-08 10:18:53 +0100

    Dashboard imported to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from './pages/Dashboard'
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={}
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
