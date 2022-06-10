057d18bcc24e2366beeba8439f873e7e6c462292
ravigordon <rgordz460@outlook.com>
2022-06-10 11:28:21 +0100

    text changed for login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,52 @@
+import {useState, useEffect} from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
 function Login() {
+    const [formData, setFormData] = useState({
+        name:'',
+        email: '',
+        pasword: '',
+        password2: ''
+    })
+    const { email, password} = formData
+
+    const onChange = (e) =>{
+        setFormData((prevState)=>(({
+
+            ...prevState,
+            [e.target.name] : e.target.value,
+            
+        })))
+
+        
+    }
+
+    const onSubmit = (e) =>{
+        e.preventDefault()
+    }
+    
   return (
-    <div>Login</div>
+    <>
+       <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login in and start setting goals</p>
+      </section>
+        <section className='form'>
+            <form onSubmit={onSubmit}>
+            
+            <div className='form-group'>
+            <input type="email" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange} />
+            </div>     
+            <div className='form-group'>
+            <input type="password" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange} />
+            </div>     
+            <div className='form-group'>
+               <button type="submit" className='btn btn-block'>Submit</button>
+            </div>         
+            </form>
+        </section>
+    </>
   )
 }
 
