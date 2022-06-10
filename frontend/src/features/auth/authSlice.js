e5e06034ef4a99a5307c2faf6deb0ec04226930f
ravigordon <rgordz460@outlook.com>
2022-06-10 12:06:54 +0100

    intial state object created

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,1 +1,9 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+const intialState ={
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
