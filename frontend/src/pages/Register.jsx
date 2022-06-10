0ced20a7f85f3213b018454020d49fba1821eb69
ravigordon <rgordz460@outlook.com>
2022-06-10 10:58:53 +0100

    Logic for onChange function added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,8 +9,15 @@ function Register() {
     })
     const {name, email, password,password2} = formData
 
-    const onChange = () =>{
+    const onChange = (e) =>{
+        setFormData((prevState)=>(({
 
+            ...prevState,
+            [e.target.name] : e.target.value,
+            
+        })))
+
+        
     }
 
     const onSubmit = (e) =>{
