d582cd4c4c2725066e5aec747dc74d4f9361ec8a
ravigordon <rgordz460@outlook.com>
2022-06-10 10:40:06 +0100

    JSX for Form completed on the Register Page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,7 +23,16 @@ function Register() {
             <form>
             <div className='form-group'>
             <input type="text" className="form-control" id="name" name="name" value={name} placeholder='Enter your name' onChange={onChange} />
+            </div> 
+            <div className='form-group'>
+            <input type="email" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange} />
             </div>     
+            <div className='form-group'>
+            <input type="password" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange} />
+            </div>     
+            <div className='form-group'>
+            <input type="password" className="form-control" id="password2" name="password2" value={password2} placeholder='Confirm Password' onChange={onChange} />
+            </div>         
             </form>
         </section>
     </>
