da56b3db349f5afb8ec7c0f0cf3cde005cec1bb0
ravigordon <rgordz460@outlook.com>
2022-06-14 11:45:15 +0100

    ToastifyContainer added to App.js

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import {useState, useEffect} from 'react'
-import {useSelector,useDispatch} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+
 function Register() {
     const [formData, setFormData] = useState({
         name:'',
