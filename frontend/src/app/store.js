4ad870af83ee30746274d64f2b94618c475bdd10
ravigordon <rgordz460@outlook.com>
2022-06-14 10:42:09 +0100

    new error message comming up on console page now renders but need to fix error in console

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,6 +3,7 @@ import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
-      auth: authReducer,
+      auth: null
   },
 });
+// new error message comming up on console page now renders
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,41 +1,41 @@
-// import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
-// import authService from './authService'
-// // Get user from localStorage
-// const user = JSON.parse(localStorage.getItem('user'))
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
+// Get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'))
 
-// const intialState ={
-//     user: user ? user : null,
-//     isError: false,
-//     isSuccess: false,
-//     isLoading: false,
-//     message: '',
-// }
-// // Register user 
-// export  const register = createAsyncThunk('auth/register', async(user,thunkAPI) => {
-//     try{
-//         return await authService.register(user)
-//     } catch(error){
-//         const message =(error.response && error.response.data && error.response.data.message) || error.message || error.toString()
-//         return thunkAPI.rejectWithValue(message)
-//     }
-// })
+const intialState ={
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+// Register user 
+export  const register = createAsyncThunk('auth/register', async(user,thunkAPI) => {
+    try{
+        return await authService.register(user)
+    } catch(error){
+        const message =(error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
 
-// export const authSlice = createSlice({
-//     name: 'auth',
-//     intialState,
-//     reducers: {
-//         reset: (state) => {
-//             state.isLoading = false
-//             state.isSuccess = false
-//             state.isError = false
-//             state.message = ''
+export const authSlice = createSlice({
+    name: 'auth',
+    intialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
             
-//         },
-//     },
-//     extraReducers: () => {
+        }
+    },
+    extraReducers: () => {
 
-//     }
-// })
-// export const { reset } = authSlice.action
-// export default authSlice.reducer
+    }
+})
+export const { reset } = authSlice.actions
+export default authSlice.reducer
 
